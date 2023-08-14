480360305af46a1a91993b5f26d6f17b023fc936
goga0301 <goga200330023@gmail.com>
2023-08-14 16:56:23 +0400

    finish organization model

--- Back/Alazani/Alazani.Domain.Models/Domain/OrganizationModel.cs
+++ Back/Alazani/Alazani.Domain.Models/Domain/OrganizationModel.cs
@@ -3,13 +3,23 @@
 namespace Alazani.Domain.Models.Domain;
 
 public record OrganizationModel(int Id,
-                         string Name,
-                         string Description,
-                         string Address,
-                         List<ProductModel>? Products,
-                         List<CategoryModel>? Categories,
-                         List<RoleModel>? Roles,
-                         List<UserModel>? Users,
-                         RecordStatusEnum RecordStatus,
-                         DateTimeOffset CreateDate,
-                         string CreatorUserId);
+                                string Name,
+                                string Description,
+                                string Address,
+                                List<ProductModel>? Products,
+                                List<CategoryModel>? Categories,
+                                List<RoleModel>? Roles,
+                                List<UserModel>? Users,
+                                RecordStatusEnum RecordStatus,
+                                DateTimeOffset CreateDate,
+                                string CreatorUserId);
+
+public record CreateOrganizationModel(string Name,
+                                      string Description,  
+                                      string Address);
+public record UpdateOrganizationModel(int Id,
+                                      string Name,
+                                      string Description,
+                                      string Address);
+public record DeleteOrganizationModel(int Id);
+
